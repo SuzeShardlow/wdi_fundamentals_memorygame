126f572b3609e8269ae5256f0b0d45bc382a9f33
Suze Shardlow <suzeplays@gmail.com>
2017-06-03 12:36:19 +0100

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,9 +1,49 @@
 console.log("Up and running!");
 
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
+var cards = ["queen", "queen", "king", "king"];
+
+var cardsInPlay = [];
+
+var cardOne = cards[0];
+
+cardsInPlay.push(cardOne);
+
+console.log("User flipped " + cardOne);
+
+var cardTwo = cards[2];
+
+cardsInPlay.push(cardTwo);
 
 console.log("User flipped " + cardTwo);
-console.log("User flipped " + cardThree);
+
+
+/* Assignment 8.12
+
+You can achieve the same thing in two different ways
+
+Option One:
+
+// if (cardsInPlay.length === 2) {
+
+//	if (cardOne === cardTwo) {
+
+//		alert('You found a match!')
+
+//	} else {
+
+//			alert('Sorry, try again.');
+
+//	}
+
+//  }
+
+Option Two: */
+
+if (cardsInPlay.length === 2) {
+
+	var matchAlert = cardsInPlay[0] === cardsInPlay[1] ? "You found a match!" : "Sorry, try again.";
+
+	alert(matchAlert);
+
+}
+
